12052973fb133305a2732d62f4d8e5e3bb6fc809
mayank1409 <mayank_srivastava@yahoo.com>
2021-11-01 11:46:34 +0530

    first hello servlet

---
+++ hello-web-application/src/main/java/io/javamasters/hello/servelet/HelloServlet.java
@@ -0,0 +1,22 @@
+package io.javamasters.hello.servelet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.write("<h1>Hello, Servlet</h1>");
+	}
+	
+}
